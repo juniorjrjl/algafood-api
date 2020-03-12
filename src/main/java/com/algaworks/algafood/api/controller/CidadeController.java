645add93c126140705a8c0bbf16c1eb299dca762
Junior <junior@DEBJR.sjk.emb>
2020-03-12 13:38:36 -0300

    documentaçao da api

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -25,6 +25,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
@@ -38,19 +43,24 @@ public class CidadeController {
     @Autowired
     private CadastroCidadeService cadastroCidade;
 
+    @ApiOperation("Lista as cidades")
     @GetMapping
     public List<CidadeModel> listar(){
         return cidadeModelAssembler.toCollectionModel(cadastroCidade.listar());
     }
 
+    @ApiOperation("Busca uma cidade por ID")
     @GetMapping("{id}")
-    public CidadeModel buscar(@PathVariable Long id){
+    public CidadeModel buscar(@ApiParam(value = "ID de uma cidade", example = "1" ) 
+        @PathVariable Long id){
         return cidadeModelAssembler.toModel(cadastroCidade.buscar(id));
     }
     
+    @ApiOperation("Cadastra uma cidade")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput){
+    public CidadeModel adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade") 
+        @RequestBody @Valid CidadeInput cidadeInput){
         try{
             Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
             return cidadeModelAssembler.toModel(cadastroCidade.salvar(cidade));
@@ -59,8 +69,13 @@ public class CidadeController {
         }
     }
 
+    @ApiOperation("Atualiza uma cidade por ID")
     @PutMapping("{id}")
-    public CidadeModel atualizar(@PathVariable Long id, @RequestBody @Valid CidadeInput cidadeInput){
+    public CidadeModel atualizar(
+        @ApiParam(value = "ID de uma cidade", example = "1" ) 
+        @PathVariable Long id, 
+        @ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados")
+        @RequestBody @Valid CidadeInput cidadeInput){
         try{
             Cidade cidadeAtual = cadastroCidade.buscar(id);
             cidadeInputDisassembler.copyToDomainInObject(cidadeInput, cidadeAtual);
@@ -71,9 +86,11 @@ public class CidadeController {
 
     }
     
+    @ApiOperation("Exclui uma cidade por ID")
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void remover(@PathVariable Long id){
+    public void remover(@ApiParam(value = "ID de uma cidade", example = "1" )  
+        @PathVariable Long id){
         cadastroCidade.excluir(id);
     }
 
--- src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 
+    @ApiModelProperty(value = "ID da cidade", example = "1")
     private Long id;
+    @ApiModelProperty(value = "Nome de uma cidade", example = "São Paulo")
     private String nome;
     private EstadoModel estado;
 }
