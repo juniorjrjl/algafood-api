d46e7933a30ad5f8251ef0e5a167a38151ee236c
José Luiz <junior.jr.jl@gmail.com>
2020-03-28 14:25:53 -0300

    configuração de cors

--- src/main/java/com/algaworks/algafood/core/security/ResourceSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceSecurityConfig.java
@@ -14,6 +14,7 @@ public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter{
         	.authorizeRequests()
         		.anyRequest().authenticated()
         	.and()
+        	.cors().and()
         	.oauth2ResourceServer().opaqueToken();
                 
     }
