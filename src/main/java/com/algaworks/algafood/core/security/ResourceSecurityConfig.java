71c27353a167408b47a3f6c95f2cdaf33b663f1c
José Luiz <junior.jr.jl@gmail.com>
2020-03-31 15:33:46 -0300

    usando jwt com chave simetrica

--- src/main/java/com/algaworks/algafood/core/security/ResourceSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.algaworks.algafood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @EnableWebSecurity
 public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -15,8 +20,14 @@ public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter{
         		.anyRequest().authenticated()
         	.and()
         	.cors().and()
-        	.oauth2ResourceServer().opaqueToken();
+        	.oauth2ResourceServer().jwt();
                 
+    }
+    
+    @Bean
+    public JwtDecoder jwtDecoder() {
+    	var secretKey = new SecretKeySpec("fsd8a7f9f47d98sa7f1sd9c879fcasdf71c8sd97cf8sad97f1csd9817cfsd977f1cd97sad19sd".getBytes(), "HmacSHA256");
+    	return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
